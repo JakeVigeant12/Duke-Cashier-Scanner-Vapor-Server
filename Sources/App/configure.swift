f4e68b7d318737977f0a13a2da10118acfea8358
Jake Vigeant <jmv43@duke.edu>
2023-12-03 09:35:57 -0500

    postgres url for heroku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,14 +9,25 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
      app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "edeposit_bag",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
+
+        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    }
+    
+//    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+//        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+//        password: Environment.get("DATABASE_PASSWORD") ?? "",
+//        database: Environment.get("DATABASE_NAME") ?? "edeposit_bag",
+//        tls: .prefer(try .init(configuration: .clientDefault)))
+//    ), as: .psql)
 
 
     app.views.use(.leaf)
