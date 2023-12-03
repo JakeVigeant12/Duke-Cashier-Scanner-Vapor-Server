b11bb930ecf0d3629cf72cff72a3d54a096cb9a4
Jake Vigeant <jmv43@duke.edu>
2023-12-03 09:51:37 -0500

    try again

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: App serve --env production --hostname 0.0.0.0 --port $PORT
+web: App serve --env production --hostname 0.0.0.0 --port $PORT --config:postgres.url=$DATABASE_URL
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,16 +19,17 @@ public func configure(_ app: Application) async throws {
 
         app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
     }
-    
-//    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-//        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
-//        password: Environment.get("DATABASE_PASSWORD") ?? "",
-//        database: Environment.get("DATABASE_NAME") ?? "edeposit_bag",
-//        tls: .prefer(try .init(configuration: .clientDefault)))
-//    ), as: .psql)
-
+    else{
+        
+            app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+                username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+                password: Environment.get("DATABASE_PASSWORD") ?? "",
+                database: Environment.get("DATABASE_NAME") ?? "edeposit_bag",
+                tls: .prefer(try .init(configuration: .clientDefault)))
+            ), as: .psql)
+    }
 
     app.views.use(.leaf)
 
